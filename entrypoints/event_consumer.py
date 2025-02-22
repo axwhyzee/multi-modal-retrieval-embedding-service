441b998c37781db8d4c31350980d76173c42a263
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-22 14:04:16 +0800

    Create event_consumer.py

---
+++ entrypoints/event_consumer.py
@@ -0,0 +1,19 @@
+import logging
+
+from event_core.adapters.pubsub import RedisConsumer
+from event_core.domain.events import ChunkStored
+
+from services.handlers import handle_chunk
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    with RedisConsumer() as consumer:
+        consumer.subscribe(ChunkStored)
+        consumer.listen(handle_chunk)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
